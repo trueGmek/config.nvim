bfdfc534746b7d1f18f1fa0118abc602bdfeda5b
trueGmek <gmek0204@gmail.com>
2024-09-27 15:58:15 +0200

    Updated tree configuration

--- init.lua
+++ init.lua
@@ -433,7 +433,7 @@ local function telescope_live_grep_open_files()
     prompt_title = 'Live Grep in Open Files',
   }
 end
-vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
+vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/]Grep in Open Files' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
@@ -610,14 +610,9 @@ local servers = {
       style = { 'file' }
     },
   },
-  cmakelang = {},
   glsl_analyzer = {},
   cmake = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  -- cmakelang = {},
 
   lua_ls = {
     Lua = {
