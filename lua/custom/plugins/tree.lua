b547d77a33f95637cb17d00a12c9f7ddd23a4414
trueGmek <gmek0204@gmail.com>
2024-09-27 15:58:15 +0200

    Updated tree configuration

--- lua/custom/plugins/tree.lua
+++ lua/custom/plugins/tree.lua
@@ -11,9 +11,24 @@ return {
     require("nvim-tree").setup {
       view = {
         width = 25,
-      }
+        adaptive_size = true,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
+          }
+        }
+      },
     }
   end,
 
-  vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeToggle<cr>')
+  vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeFindFileToggle!<cr>'),
+  vim.keymap.set('n', '<leader>tft', '<cmd>NvimTreeFindFileToggle!<cr>', { desc = "[Tree] [F]ind file [Toggle]" }),
+  vim.keymap.set('n', '<leader>tff', '<cmd>NvimTreeFindFile!<cr>', { desc = "[T]ree [F]ind [F]ile" }),
 }
